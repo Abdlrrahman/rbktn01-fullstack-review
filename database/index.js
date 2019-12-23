51d625454bf2a521973c6a271be794289588f394
Abdlrrahman <abdlrrahman.shibani@gmail.com>
2019-12-23 15:57:44 +0100

    1

--- database/index.js
+++ database/index.js
@@ -3,14 +3,18 @@ mongoose.connect('mongodb://localhost/fetcher');
 
 let repoSchema = mongoose.Schema({
   // TODO: your schema here!
+  userId: INT,
+  userName: text,
+  userRepositoriesNames: text
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
+let save = (/* TODO */ repo) => {
   // TODO: Your code here
   // This function should save a repo or repos to
   // the MongoDB
+  repoSchema.userRepositoriesNames = repo;
 }
 
 module.exports.save = save;
